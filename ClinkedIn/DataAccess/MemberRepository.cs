24a57c6ff1d27a9bd6ac581c42bfd71f30ee457b
Chris LoJacono <clojacono92@gmail.com>
2021-03-18 20:06:35 -0500

    saving progress

--- ClinkedIn/DataAccess/MemberRepository.cs
+++ ClinkedIn/DataAccess/MemberRepository.cs
@@ -10,60 +10,7 @@ namespace ClinkedIn.DataAccess
 {
     public class MemberRepository
     {
-        static List<Member> _allMembers = new List<Member>
-            {
-                new Member {
-                    InmateId = 1,
-                    Name="Prison Mike",
-                    MemberInterests = new List<Interest>
-                    {
-                       new Interest{ Id = 1, Interests = "Playing Spades", Type= InterestType.Gaming},
-                       new Interest{ Id = 2, Interests = "Playing Solitare", Type= InterestType.Gaming},
-                       new Interest{ Id = 3, Interests = "Lifting Weights", Type= InterestType.Exercise},
-                    },
-                    MemberServices = new List<Service>
-                    {
-                        new Service{ Id = 1, Services = "Cleaning blood"},
-                        new Service{ Id = 2, Services = "Creating blood"},
-                        new Service{ Id = 3, Services = "Starting Fight"}
-                    },
-                    Sentence = 420,
-                },
-                new Member {
-                    InmateId = 2,
-                    Name="Martha Stewart",
-                    MemberInterests = new List<Interest>
-                    {
-                       new Interest{ Id = 4, Interests = "Slam Poetry", Type= InterestType.Arts},
-                       new Interest{ Id = 2, Interests = "Playing Solitare", Type= InterestType.Gaming},
-                       new Interest{ Id = 6, Interests = "Doing Push-ups", Type= InterestType.Exercise},
-                    },
-                    MemberServices = new List<Service>
-                    {
-                        new Service{ Id = 1, Services = "Shanking a Guard"},
-                        new Service{ Id = 2, Services = "Personal Trainer"},
-                        new Service{ Id = 3, Services = "Manufacturing Weapons"}
-                    },
-                    Sentence = 200,
-                },
-                new Member {
-                    InmateId = 3,
-                    Name="Ghosteface Killa",
-                    MemberInterests = new List<Interest>
-                    {
-                       new Interest{ Id = 4, Interests = "Slam Poetry", Type= InterestType.Arts},
-                       new Interest{ Id = 7, Interests = "Juggling Shanks", Type= InterestType.Arts},
-                       new Interest{ Id = 8, Interests = "Community Puppet Shows", Type= InterestType.Arts},
-                    },
-                    MemberServices = new List<Service>
-                    {
-                        new Service{ Id = 1, Services = "Laundry"},
-                        new Service{ Id = 2, Services = "Back Massage"},
-                        new Service{ Id = 3, Services = "Shower Assistant"}
-                    },
-                    Sentence = 404,
-                },
-            };
+        
 
         const string ConnectionString = "Server=localhost;Database=ClinkedIn;Trusted_Connection=True";
 
@@ -83,10 +30,16 @@ namespace ClinkedIn.DataAccess
 
         public void AddAMember(Member member)
         {
-            var biggestExistingId = _allMembers.Max(memb => memb.InmateId);
-            member.InmateId = biggestExistingId + 1;
-            _allMembers.Add(member);
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"INSERT INTO [dbo].[Members]([Name],[Sentence])
+                        OUTPUT inserted.Id
+                        VALUES(@Name,@Sentence)";
+            var id = db.ExecuteScalar<int>(sql, member);
+
+            member.InmateId = id;
         }
+
         public Member GetAMember(int id)
         {
             var sql = @"select *
@@ -103,7 +56,7 @@ namespace ClinkedIn.DataAccess
         public void RemoveAMember(int id)
         {
             var memberToRemove = GetAMember(id);
-            _allMembers.Remove(memberToRemove);
+           // _allMembers.Remove(memberToRemove);
         }
 
         public void AddEnemy(int inmateId, int enemyId)
